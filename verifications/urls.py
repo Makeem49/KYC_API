2af8c5f19635b24fe1f424bb2901a51ce2c4a051
Moshood Akeem Olayinka <amoshood@africaexchange.com>
2023-07-29 12:03:46 +0100

    remove url

--- verifications/urls.py
+++ verifications/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (CustomerDetailView, SMSLinkView, OkraWebhookEventNotification, 
+from .views import (CustomerDetailView, SMSLinkView,  
                     CustomerListView, CustomerUpdateView, WaitListView, RiskThresholdView, UdateRiskThresholdView)
 
 urlpatterns = [
@@ -7,7 +7,6 @@ urlpatterns = [
     path('customers/<int:pk>/', CustomerDetailView.as_view(), name='customer_detail_view'),
     path('customers/<int:pk>/update/', CustomerUpdateView.as_view(), name='customer_update_view'),
     path('user/sms/', SMSLinkView.as_view(), name='customer_detail_view'), 
-    path('notifications/', OkraWebhookEventNotification.as_view(), name='notification_view'),
     path('waitlist/', WaitListView.as_view(), name='wait_list_view'),
     path('risk_threshold/', RiskThresholdView.as_view(), name='risk_threshold'),
     path('risk_threshold_update/<int:pk>', UdateRiskThresholdView.as_view(), name='risk_threshold_update'),
