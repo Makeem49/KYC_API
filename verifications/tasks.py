b3e377232c9ab5bcbf9044c1d34716d4179746bd
Moshood Akeem Olayinka <amoshood@africaexchange.com>
2023-09-11 07:29:28 +0100

    updated

--- verifications/tasks.py
+++ verifications/tasks.py
@@ -6,8 +6,12 @@ from app.celery import app
 @shared_task
 def send_link_message(phone_number):
     message = """
-    Hello from simbard HQ, Kindly authorize us your account information by clicking on the below link. 
-    Click the below link, fill the form with correct information. 
+    As Salam Alaikum, you've requested to perform the KYC by AlQasswa to issue your Umrah Visa. We need to verify:
+    1. Employment Status
+    2. Income Status
+    3. Account Balance
+
+    Please complete the form below by clicking on the link below.
 
     https://app.creditchek.africa/customer/onboarding?type=short&appId=5293878414&appLink=eFd1ZNdJda&app_id=64aac9d453a97b63508946e7&status=true
     
